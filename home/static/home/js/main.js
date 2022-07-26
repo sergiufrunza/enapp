a3bb969929e8251bd8a6b4c5c7fb026fb77ee9df
sergiufrunza <sergiu.frunza120@gmail.com>
2022-07-26 20:05:48 +0300

    Front-end v1.1

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -20,7 +20,11 @@ const navItem = document.getElementsByClassName("nav_item_name");
 const LeftMenuIt = document.getElementsByClassName("icon_nav_item");
 const LeftMenuContainer = document.getElementById("nav_items");
 const ProfileFoto = document.getElementById("profile_foto");
-
+const textareaContainer = document.getElementById("answer");
+const check_btn = document.getElementById("btn_check");
+const md_txt = document.getElementById("md_text");
+const btn_log = document.getElementById("btn_log");
+const reset_btn = document.getElementById("btn_reset");
 
 function delay2(){
     let i = 0;
@@ -29,9 +33,59 @@ function delay2(){
        navItem[i].style.display = "block";
    }
 }
-containerBurger.onclick = function click(){
-let i = 0;
-        if (LeftMenu.style.width === "250px"){
+
+function login(){
+    $.ajax({
+            type: "GET",
+            url: '/',
+            data: {
+                logged: true
+             },
+             dataType: 'json'
+             });
+            window.location.href = '/?logged=true';
+}
+
+function get_info(){
+    let en = textareaContainer.value;
+    let md = md_txt.innerHTML;
+    textareaContainer.value = "";
+        $.ajax({
+            type: "GET",
+            url: window.location.href,
+            data: {
+                md: md,
+                en: en
+             },
+             dataType: 'json'
+             });
+            if (window.location.href.indexOf('logged') != -1){
+                window.location.href = window.location.href + '&md=' + md + '&en=' + en
+            }
+            else {
+                window.location.href = '/'
+            }
+
+
+}
+
+check_btn.onclick = function(){
+    get_info()
+            }
+if (btn_log){
+btn_log.onclick = function(){
+    login()
+}
+}
+    reset_btn.onclick = function() {
+        textareaContainer.value = "";
+        window.location.href="/?logged=true";
+
+}
+if (!btn_log) {
+    containerBurger.onclick = function click() {
+        let i = 0;
+        if (LeftMenu.style.width === "250px") {
             LeftMenuContainer.style.width = null;
             LeftMenu.style.width = "50px";
             ProfileFoto.style.width = "46px"
@@ -41,12 +95,11 @@ let i = 0;
             Burger.classList.toggle("burger");
             Burger.classList.toggle("burger_active");
             profile.style.display = "none";
-            for (i  in navItem){
-                 LeftMenuIt[i].style.justifyContent = "end";
+            for (i in navItem) {
+                LeftMenuIt[i].style.justifyContent = "end";
                 navItem[i].style.display = "none";
             }
-        }
-        else{
+        } else {
             LeftMenuContainer.style.width = "100%";
             LeftMenu.style.width = "250px";
             ProfileFoto.style.width = "75px"
@@ -55,12 +108,12 @@ let i = 0;
             LeftMenu.style.alignItems = "flex-start";
             Burger.classList.toggle("burger_active");
             Burger.classList.toggle("burger");
-            setTimeout(delay2 ,200);
-            for (i  in LeftMenuIt){
+            setTimeout(delay2, 200);
+            for (i in LeftMenuIt) {
                 LeftMenuIt[i].style.justifyContent = "start";
             }
         }
         return 0;
-};
-
+    };
+}
 
--- home/views.py
+++ home/views.py
@@ -1,7 +1,62 @@
 
-from django.shortcuts import render
-
-
+from django.shortcuts import *
+from django.http import JsonResponse, HttpResponse
+from django.template import RequestContext
+from . import source
+import random
+import json
 def index(request):
-    return render(request, 'home/homepage.html')
-# Create your views here.
+    x = random.randint(0, len(source.dictionary)-1)
+    if request.GET.get("en"):
+        geten = request.GET.get("en")
+        getmd = request.GET.get("md")
+        data = checkwords(geten, getmd)
+        data['logged'] = True
+        return render(request, 'home/homepage.html', context=data)
+
+    elif request.GET.get('logged') and not request.GET.get("en"):
+        data = {}
+        data['logged'] = True
+        data['md'] = source.dictionary[x]["md"]
+        return render(request, 'home/homepage.html', context=data)
+    else:
+        data = {
+            "md": source.dictionary[x]["md"]
+        }
+        return render(request, 'home/homepage.html')
+
+
+
+
+
+
+
+def checkwords(en ,md):
+
+    getenarray = en.split()
+    for a in source.dictionary:
+        if a['md'] == md:
+            getenoriginarray = a['en'].split()
+            if len(getenarray) > len(getenoriginarray) or len(getenarray) == len(getenoriginarray):
+                arrayparent = getenoriginarray
+            else:
+                arrayparent = getenarray
+
+            for b in range(len(arrayparent)):
+                if getenarray[b] != getenoriginarray[b]:
+                    getenarray[b] = {
+                        "correct": False,
+                        "word": getenarray[b]
+                    }
+                else:
+                    getenarray[b] = {
+                        "correct": True,
+                        "word": getenarray[b]
+                    }
+
+
+    data = {
+        "md": md,
+        "en": getenarray
+    }
+    return data
